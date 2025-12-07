c27b645799c6432a440953f7e2d35be0ac4d0d71
BHAVENTHAN <bhaventhan08@gmail.com>
2025-12-07 23:04:01 +0530

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 
+student_name = "BHAVENTHAN R"
+unique_id = "42130081"
 topic = "home/bhaventhanr-2025/sensor"
 
 def on_message(client, userdata, msg):
@@ -12,3 +14,4 @@ client.subscribe(topic)
 client.on_message = on_message
 
 client.loop_forever()
+
